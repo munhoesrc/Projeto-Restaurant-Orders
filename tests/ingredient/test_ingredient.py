c2a8e0d9d9c46813df760d7a05fdb3c867c0c736
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-06-22 08:50:19 -0300

    feat: implementando teste para classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient('queijo mussarela')
+
+    assert ingredient.name == 'queijo mussarela'
+
+    excepted_restrictions = {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+    }
+    assert ingredient.restrictions == excepted_restrictions
+
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
+
+    ingredient1 = Ingredient('queijo mussarela')
+    assert ingredient == ingredient1
+
+    assert hash(ingredient) == hash('queijo mussarela')
+
+    ingredient2 = Ingredient("queijo parmesão")
+    assert ingredient != ingredient2
+
+    assert hash(ingredient) != hash(ingredient2)
