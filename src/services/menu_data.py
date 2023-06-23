1d3ebcc6ff7cbf2b77825a9e7dd744771e003f15
Renan Casarin Munhoes <munhoesrc@gmail.com>
2023-06-23 10:33:36 -0300

    feat: implementando a classe MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,35 @@
 # Req 3
+import csv
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = self._load_menu_data(source_path)
+
+    def _load_menu_data(self, source_path: str) -> set:
+        dishes = set()
+
+        with open(source_path) as file:
+            reader = csv.reader(file)
+            header, *data = reader
+
+            for item in data:
+                name, price, ingredient_name, quantity = item[:4]
+
+                dish = self._get_or_create_dish(dishes, name, float(price))
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(ingredient, int(quantity))
+
+        return dishes
+
+    def _get_or_create_dish(
+            self, dishes: set, name: str, price: float) -> Dish:
+        for dish in dishes:
+            if dish.name == name:
+                return dish
+
+        new_dish = Dish(name, price)
+        dishes.add(new_dish)
+        return new_dish
